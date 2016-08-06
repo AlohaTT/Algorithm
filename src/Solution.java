6a31d32b03f4e49d6e6e69a8a1ee1cf1e793a6be
Tony <1414596386@qq.com>
2016-08-06 21:17:21 +0800

    一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,24 @@
+/*
+ * 一个整型数组里除了两个数字之外，其他的数字都出现了两次。
+ * 请写程序找出这两个只出现一次的数字。 
+ */
+import java.util.HashMap;
+import java.util.Map;
 
+//num1,num2分别为长度为1的数组。传出参数
+//将num1[0],num2[0]设置为返回结果
 public class Solution {
-	
+	public void FindNumsAppearOnce(int[] array, int num1[], int num2[]) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < array.length; i++) {
+			if (map.containsKey(array[i])) {
+				map.remove(array[i]);
+
+			} else
+				map.put(array[i], 1);
+		}
+		Object[] a = map.keySet().toArray();
+		num1[0] = (int) a[0];
+		num2[0] = (int) a[1];
+	}
 }
