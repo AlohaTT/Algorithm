914a99448ed27575d865c03dc56a6b18579876f5
AlohaTT <1414596386@qq.com>
2016-09-09 19:07:46 +0800

    操作给定的二叉树，将其变换为源二叉树的镜像。 输入描述: 二叉树的镜像定义：源二叉树     	    8     	   /  \     	  6   10     	 / \  / \     	5  7 9 11     	镜像二叉树     	    8     	   /  \     	  10   6     	 / \  / \     	11 9 7  5

---
+++ src/TreeNode.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author tony
+ *
+ */
+public class TreeNode {
+
+	/**
+	 * 
+	 */
+	int val = 0;
+	TreeNode left = null;
+	TreeNode right = null;
+
+	public TreeNode(int val) {
+		this.val = val;
+
+	}
+
+}
